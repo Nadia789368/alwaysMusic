f1a716edf2642f95dc3614d8d107385acf836aa5
jose <joseopazo.86@gmail.com>
2020-12-28 15:43:30 -0300

    funcion registrar

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { Client } = require("pg");
-const argumentos = process.argv.slice(2)
+const argumentos = process.argv.slice(2);
+const nombre = argumentos[0];
+const rut = argumentos[1];
+const curso = argumentos[2];
+const nivel = argumentos[3];
 
 const config = {
   user: "postgres",
@@ -13,17 +17,19 @@ const client = new Client(config);
 
 client.connect();
 
-async function registrar(){
-    const res = await client.query()
+async function registrar(nombre, rut, curso, nivel) {
+  const res = await client.query(
+    `insert into estudiantes (nombre,rut, curso, nivel ) values ('${nombre}', '${rut}','${curso}','${nivel}')`
+  );
+
+  client.end();
+}
+registrar(nombre, rut, curso, nivel);
+
+async function eliminar() {
+  const res = await client.query();
 }
 
-
-async function eliminar(){
-    const res = await client.query()
+async function consultar() {
+  const res = await client.query();
 }
-
-
-async function consultar(){
-    const res = await client.query()
-}
-
