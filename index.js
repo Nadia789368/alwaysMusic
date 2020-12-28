ab3f01b84a0cc8b2252e720281a26d7194674c24
PedroManuelRivas <pedromanuelrivas@gmail.com>
2020-12-28 17:04:41 -0300

    Requerimientos 5y6 (Editar y Eliminar)

--- index.js
+++ index.js
@@ -6,31 +6,41 @@ const config = {
   user: "postgres",
   host: "localhost",
   database: "alwaysmusic",
-  password: "postgre",
+  password: "postgres",
   port: 5432,
 };
 
 const client = new Client(config);
 client.connect();
 
-(async ()=>{
-  if(accion == 'nuevo'){
-    let nombre = argumentos[1]
-    let rut = argumentos[2]
-    let curso = argumentos[3]
-    let nivel = argumentos[4]
-    await registrar(nombre, rut, curso, nivel)
-  }
-  else if(accion == 'rut'){
-    let rut = argumentos[1]
-    await consultar(rut)
-  }
-  else if(accion == 'consulta'){
-    await consultarTodos()
-  }
-  else console.log("El comando ingresado no es correcto, vuelve a intentar")
-client.end()
-})()
+(async () => {
+  if (accion == "nuevo") {
+    let nombre = argumentos[1];
+    let rut = argumentos[2];
+    let curso = argumentos[3];
+    let nivel = argumentos[4];
+    await registrar(nombre, rut, curso, nivel);
+  } else if (accion == "rut") {
+    let rut = argumentos[1];
+    await consultar(rut);
+  } else if (accion == "consulta") {
+    await consultarTodos();
+  } else if (accion == "actualizar") {
+    let nombre = argumentos[1];
+    let rut = argumentos[2];
+    await actualizar(nombre, rut);
+  } else if (accion == "eliminar") {
+    let rut = argumentos[1];
+    await eliminar(rut);
+  } else if (accion == "updateCampos") {
+    let rut = argumentos[1];
+    let campoActualizar = argumentos[2];
+    let campoNuevo = argumentos[3];
+    await updateCampos(rut, campoActualizar, campoNuevo);
+    console.log(`Campo actualizado ${campoNuevo}`);
+  } else console.log("El comando ingresado no es correcto, vuelve a intentar");
+  client.end();
+})();
 
 async function registrar(nombre, rut, curso, nivel) {
   const res = await client.query(
@@ -38,18 +48,33 @@ async function registrar(nombre, rut, curso, nivel) {
   );
 }
 
-async function eliminar() {
-  const res = await client.query();
+async function updateCampos(rut, campoActualizar, campoNuevo) {
+  await client.query(
+    `UPDATE estudiantes SET ${campoActualizar} = '${campoNuevo}' WHERE rut = '${rut}' returning *;`
+  );
+}
+
+async function actualizar(nombre, rut) {
+  const res = await client.query(
+    `UPDATE estudiantes SET nombre = '${nombre}' WHERE rut = '${rut}' returning *;`
+  );
+  console.log(`Datos de estudiante ${nombre}, actualizado`, res.rows);
+}
+
+async function eliminar(rut) {
+  const res = await client.query(
+    `DELETE FROM estudiantes WHERE rut = '${rut}'`
+  );
 }
 
 async function consultar(rut) {
-  const res = await client.query(`select * from estudiantes where rut = '${rut}'`);
-  console.log("Se ha encontrado un estudiante", res.rows)
+  const res = await client.query(
+    `select * from estudiantes where rut = '${rut}'`
+  );
+  console.log("Se ha encontrado un estudiante", res.rows);
 }
 
-async function consultarTodos(){
-  const res = await client.query("select * from estudiantes")
-  console.log(res.rows)
+async function consultarTodos() {
+  const res = await client.query("select * from estudiantes");
+  console.log(res.rows);
 }
-
-
